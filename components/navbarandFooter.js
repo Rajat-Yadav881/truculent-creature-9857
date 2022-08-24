9d5ea1c3ef2fc028e178dfc9dc96549fcad8f5f3
runforest881 <yyrajat881@gmail.com>
2022-08-24 09:50:58 +0530

    lk

---
+++ components/navbarandFooter.js
@@ -0,0 +1,105 @@
+function navbar(){
+    return `<div id="mil_class_set">
+    <div id="img_class_set">
+        <img  src="./components/img/pic1.png" alt="Error">
+    </div>
+    <div id="set_class_inner">
+        <div id="navsection_class_set">
+            <div id="navsection_class_set_feel_1" class="section_one"><a href="#">Home</a> </div>
+            <div id="navsection_class_set_feel_1"><a href="#">Donate</a></div>
+            <div id="navsection_class_set_feel_1"><a href="#">Lend</a></div>
+            <div id="navsection_class_set_feel_1"><a href="#">Pricing</a></div>
+            <div id="navsection_class_set_feel_1"><a href="#">Contact us</a></div>
+            <div id="navsection_class_set_feel_1" class="op">
+            <div class="toggle">
+        <input type="checkbox" id="temp">
+        <label for="temp"></label>
+      </div>
+            </div>
+        </div>
+        <div id="fundReiser"><a href="#">Start fundraiser</a></div>
+    </div>
+    <div id="profileSet"><a href="#">login</a></div>
+</div>`
+}
+function Footer(){
+    return`
+    <div>
+        <div>
+            <span>Donate Toward</span>
+            <a href=""><div>
+                <img src="" alt="Error">
+                <p>Medical</p>
+             </div></a>
+            <a href=""><div>
+                <img src="" alt="Error">
+               <p>Education</p>
+            </div></a>
+            <a href=""><div>
+                <img src="" alt="Error">
+               <p>Memorial</p>
+            </div></a>
+        </div>
+        <div>
+            <span>Milaap</span>
+            <div><a href="#">About us</a></div>
+            <div><a href="#">Press and Media</a></div>
+            <div><a href="#">Team</a></div>
+            <div><a href="#">Careers</a></div>
+            <div><a href="#">Contacts</a></div>
+            <div><a href="#">Board</a></div>
+            <div><a href="#">Thank You</a></div>
+        </div>
+        <div>
+            <span>indian office adress</span>
+            <p>
+                "Milaap Social Ventures India Pvt. Ltd."
+                <br>
+                "ClayWorks Create - building,11th KM Create Campus,Arakere Bannerghatta Rd,"
+                <br>
+                "Bangalore, Karnataka, India 560076"
+            </p>
+            <span>Supported-by</span>
+            <div>
+                <div><a href="#"><img src="" alt="Error"></a></div>
+                <div><a href="#"><img src="" alt="Error"></a></div>
+                <div><a href="#"><img src="" alt="Error"></a></div>
+                <div><a href="#"><img src="" alt="Error"></a></div>
+            </div>
+        </div>
+        <div>      
+            <div>
+                <button href="#">Start a fundraiser</button>
+                <div>
+                    <a href="#"><p>Pricing</p></a>
+                    <a href="#"><p>Reviews</p></a>
+                    <a href="#"><p>FAQs and tips</p></a>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>Find us on</p>
+                    <div>
+                        <div><a href="#"><img src="" alt="Error"></a></div>
+                        <div><a href="#"><img src="" alt="Error"></a></div>
+                        <div><a href="#"><img src="" alt="Error"></a></div>
+                        <div><a href="#"><img src="" alt="Error"></a></div>
+                        <div><a href="#"><img src="" alt="Error"></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img src="" alt="Error">
+            </div>
+            <div>
+                <p>Set up, manage and promote your fundraiser with Milaap app</p>
+                <span>Download Now!</span>
+                <div><a href="#"><img src="" alt="Error"></a></div>
+                <div><a href="#"><img src="" alt="Error"></a></div>
+            </div>
+        </div>
+    </div>`
+}
+export {navbar,Footer}
