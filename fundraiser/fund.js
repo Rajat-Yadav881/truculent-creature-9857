a8cff742c54ffa7e47a48b7f8ec857fd27ca62c3
runforest881 <yyrajat881@gmail.com>
2022-08-28 11:46:09 +0530

    lk

--- fundraiser/fund.js
+++ fundraiser/fund.js
@@ -1,24 +1,41 @@
 
 
-
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0]
-function Close()
+​
+let x=document.getElementById("otpbutton").addEventListener('click',getData)
+ let arr=[]
+function getData()
 {
-  modal.style.display = "block";
+  let obj={
+    name:document.getElementById('name').value,
+    email:document.getElementById('email').value,
+    number:document.getElementById('password').value
+  }
+  if(obj.email=="xyz@gmail.com" || obj.email == "abc@gmail.com")
+  {
+    alert('we have sent an otp to your Email')
+    modal.style.display = "block";
+  }
+  else if(obj.name =='' || obj.email =='' || obj.number == '')
+  {
+    alert('your input field is empty')
+  }
+​
+​
+ 
 }
-
-
+​
 span.onclick = function() {
   modal.style.display = "none";
 }
-
+​
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-
+​
 function btn1()
 {
   document.querySelector(".arrow-dropdown").innerText="Medical"
@@ -35,17 +52,19 @@ function btn4()
 {
   document.querySelector(".arrow-dropdown").innerText="Others"
 }
-
-
-let otp=document.getElementById("otp");
-    let button=document.getElementById("otpbutton");
-    button.addEventListener("click",()=>{
-        if(otp.value==1234){
-            alert("Subscription Successful..😎");
-            window.location.href="../homepage/index.html";
+​
+function getOtp()
+{
+  let otp=document.getElementById("otp");
+    
+        if(otp.value==123456){
+            alert("Thankyou for fund in the milaap..😊");
+            // window.location.href="index.html"
         }
         else{
             alert("Wrong OTP..😒")
         }
-
-    })
+​
+    
+}
+​
